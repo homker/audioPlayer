209f69c301a2f9eda45dc8c9d0dbe83bc2bf9e48
homker <liu15jia@163.com>
2014-10-31 20:44:30 +0800

    add loading

--- js/audio.js
+++ js/audio.js
@@ -1,7 +1,21 @@
 "use strict";
-var player = document.getElementsByTagName("audio");
-var src  =  document.getElementsByTagName("source");
-if (!player) {
-	player  = new Audio();
-};
+window.onload =function(){
+	var player = document.getElementsByTagName("audio");
+	var src  =  document.getElementsByTagName("source");
+	var url = src[0].src;
+	var type = src[0].type;
+	var play = document.getElementById("play-pause").childNodes[1];
+	if (!player) {
+		player  = new Audio();
+	}else{
+		player = player[0];
+	}
+	//console.log(play.childNodes[1].className.replace("glyphicon-play","glyphicon-pause"));
+	if(player.canPlayType(type)){
+		player.src = url;
+		play.className = play.className.replace("glyphicon-play","glyphicon-loading");
+	}else{
+		return false;
+	}
+}
 
