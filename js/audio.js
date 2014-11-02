09cf4a26da3d9241dfdf01938affdbcd7b47ad60
homker <liu15jia@163.com>
2014-11-02 21:08:22 +0800

    fix

--- js/audio.js
+++ js/audio.js
@@ -8,8 +8,18 @@ window.onload =function(){
 	play = playButton.childNodes[1],
 	progressbar = document.getElementsByName("progressbar")[0],
 	loadprogressbar = document.getElementsByName("loadprogressbar")[0],
+	timebar = document.getElementsByName("timebar")[0],
 	 playType,
-	 start = false;
+	 start = false,
+	 secondsToTime = function( secs ){
+		var hoursDiv = secs / 3600, hours = Math.floor( hoursDiv ), minutesDiv = secs % 3600 / 60, minutes = Math.floor( minutesDiv ), seconds = Math.ceil( secs % 3600 % 60 );
+		if( seconds > 59 ) { seconds = 0; minutes = Math.ceil( minutesDiv ); }
+		if( minutes > 59 ) { minutes = 0; hours = Math.ceil( hoursDiv ); }
+		return ( hours == 0 ? '' : hours > 0 && hours.toString().length < 2 ? '0'+hours+':' : hours+':' ) + ( minutes.toString().length < 2 ? '0'+minutes : minutes ) + ':' + ( seconds.toString().length < 2 ? '0'+seconds : seconds );
+	},
+	changeClassName = function(obj, firstName, secondName){
+		obj.className = obj.className.replace(firstName,secondName);
+	}
 	 player = (!player)?new Audio() : player[0];
 	if(player.canPlayType(type)){
 		player.src = url;
@@ -19,40 +29,35 @@ window.onload =function(){
 	}
 	playButton.addEventListener("click",function(){
 		if (playType&&start==false) {
-			play.className = play.className.replace("glyphicon-play","glyphicon-loading");
+			changeClassName(play,"glyphicon-play","glyphicon-loading");
 			start = true;
 			player.play();
 		}else if(start == true){
 			player.pause();
-			play.className = play.className.replace("glyphicon-pause","glyphicon-play");
+			changeClassName(play,"glyphicon-pause","glyphicon-play");
 			start = false;
 		}
 	},false);
 	player.addEventListener('canplay',function(){
-		play.className = play.className.replace("glyphicon-loading","glyphicon-pause");
+		changeClassName(play,"glyphicon-loading","glyphicon-pause");
 	},false);
 	player.addEventListener('playing',function(){
-		play.className = play.className.replace("glyphicon-loading","glyphicon-pause");
+		changeClassName(play,"glyphicon-loading","glyphicon-pause");
 	},false);
 	player.addEventListener('ended',function(){
-		play.className = play.className.replace("glyphicon-pause","glyphicon-play");
+		changeClassName(play,"glyphicon-pause","glyphicon-play");
 		progressbar.style.width = 0 + "%";
 		start = false;
 	},false);
+	console.log(timebar);
 	setInterval(function(){
 		var  loaded = Math.floor(player.buffered.end(0) / player.duration  * 100 );
 		var played = Math.floor(player.played.end(0) / player.duration * 100);
 		progressbar.style.width = played + "%";
 		loaded = loaded - played;
+		console.log(secondsToTime(played));
+		timebar.innerHTML = secondsToTime(played) + "/"  + secondsToTime(player.duration);
 		loadprogressbar.style.width = loaded + "%";
 	},100);
-	var allPrpos=function(obj){
-		var props = "";
-		for(var p in obj){
-			if(typeof(obj[p]) == "function"){
-				console.log(obj[p]);
-			}else continue;
-		}
-	}
 }
 
